38f8b33f1c54bbddc052d82b1f59c3a047d9ed9a
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-03-30 22:12:25 +0600

    navbar part complete

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,12 +1,20 @@
-    const folders = document.querySelectorAll('.folder-dropdown');
-    
-    folders.forEach((folderContent)=> {
-        const folder = document.querySelector('.folder-content');
-        const icon = document.getElementById('icon')
-       
-        folderContent.addEventListener('click',()=> {
-        folder.style.display = folder.style.display === 'none' ? 'block' : 'none';
-        icon.style.transform = icon.style.transform === 'rotate(0deg)' ? 'rotate(180deg)' : 'rotate(0deg)'
+const folders = document.querySelectorAll('.folder-dropdown');
+
+folders.forEach((folderContent, index) => {
+    const folders = document.querySelectorAll('.folder-content');
+    const icon = document.querySelectorAll('.fa-caret-up')
+
+    folderContent.addEventListener('click', () => {
+        icon[index].style.transform = icon[index].style.transform === 'rotate(0deg)' ? 'rotate(180deg)' : 'rotate(0deg)'
+        folders[index].style.display = folders[index].style.display === 'none' ? 'flex' : 'none';
+
+
     })
-    })
+    // folderContent.addEventListener('click', () => {
+    //     folders.forEach((folder, idx, num) => {
+    //         num[idx].style.display = num[idx].style.display === 'none' ? 'flex' : 'none';
+    //     })
+    //    
+    // })
+})
 
